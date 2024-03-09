05d09f7d062a5d15be19f5b6cd3b5d24055b0ab4
HuyenTran <huyen2002@gmail.com>
2024-03-09 23:25:06 +0700

    update chat group send messsages

--- src/RouterDom/pages/Home/areaRight/ChatBox/ChatSingleSend.tsx
+++ src/RouterDom/pages/Home/areaRight/ChatBox/ChatSingleSend.tsx
@@ -1,15 +1,21 @@
-import React, { FunctionComponent, useState } from 'react'
+import React, { FunctionComponent, useContext, useEffect, useState } from 'react'
 import { ScrollChat } from './ScrollChat'
 import { Input, Spin } from 'antd'
 import { AiOutlineTags } from 'react-icons/ai'
 import { MdKeyboardArrowLeft } from 'react-icons/md'
+import { UserContext } from '../../../../../Context/UserContext'
 
 export const ChatSingleSend:FunctionComponent<any>=({selectedChat})=> {
     const [inputValue, setInputValue] = useState('');
+    const contextUser = useContext(UserContext)
+    const { state } = contextUser
+    const [user, setUser] = state.user
+    const {socket}=state
+    const [wordchat,setwordChat]=useState('')
+    const [loadingSelectChat,setLoadingSelectChat]=useState(false)
+    const [loadingsending,setLoadingsending]=useState(false);
     const getSelectUserIsChoose = (selectedChat: any) => {
-
         return (
-     
           <>
             <div className='flex items-center p-1'>
               <p className='presspreveriose' ><MdKeyboardArrowLeft size={30} className='text-gray-600 cursor-pointer -mr-3' /></p>
@@ -28,14 +34,45 @@ export const ChatSingleSend:FunctionComponent<any>=({selectedChat})=> {
         )
       }
 
-      const handleChange = (event: any) => {
-        
-      };
+     
+
+      useEffect(() => {
+        if(socket){
+          socket.on("updatedSendThread",(data:any)=>{
+            submitChatSuccess(data);
+            console.log(data)
+            setTimeout(()=>{
+              setLoadingsending(false)
+              setwordChat('')
+            },2000)
+          })
+         return ()=>{
+           socket.off('updatedSendThread')
+         }
+        }
+      },[]) 
+
       const handleKeyPress = async (event: any) => {
+        if (event.key === 'Enter') {
+            const Thread={
+              messages:{
+                message:event.target.value
+              },
+              userId:user.id,
+              chatId:selectedChat.id,      
+              // token:token
+            }
+            if(socket){
+               socket.emit('sendThread',Thread)
+            }
+            setwordChat(event.target.value)
+            event.target.value = '';
+            setLoadingsending(true)
+            setInputValue('')
+        }
       };
+
       const submitChatSuccess = (data: any) => {
-    
-       
     
       }
   return (
@@ -49,16 +86,13 @@ export const ChatSingleSend:FunctionComponent<any>=({selectedChat})=> {
         className='text-black text-4xl m-auto justify-center self-center  ' /> : */} 
         <ScrollChat />
       {/* {isTyPing?<img src={`${imageTyping}` } className='w-6 h-6 rounded-3xl'/>:""}  */}
-      <Input
-        onKeyPress={handleKeyPress}
-        onChange={handleChange}
-
-        placeholder='Nhập @, tin nhắn mới ???'
-
-        value={inputValue}
-        className='rounded-none h-14 w-full placeholder-gray-500'
-
-      />
+          <Input
+                onChange={(e) => {setInputValue(e.target.value)}}
+                onKeyPress={handleKeyPress}
+                placeholder='Nhập @, tin nhắn mới ???'
+                value={inputValue}
+                className='rounded-none h-14 w-full  placeholder-gray-500 to-black'
+              />
     </div>
   </div>
   )
